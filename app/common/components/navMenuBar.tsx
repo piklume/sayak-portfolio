734f6b37acc79474fb94ac8f8685b767daf10d56
Sayak Dutta <sayak_dutta@intuit.com>
2023-06-02 02:47:22 +0530

    fix: change header colors

--- app/common/components/navMenuBar.tsx
+++ app/common/components/navMenuBar.tsx
@@ -8,7 +8,7 @@ import { MENU_LIST } from '../constants';
 const NavMenuBar = () => {
   return (
     <NavigationMenu.Root className="relative z-[1] flex w-screen justify-center">
-      <NavigationMenu.List className="center shadow-blackA7 m-0 flex list-none rounded-[6px] bg-white p-1 shadow-[0_2px_10px]">
+      <NavigationMenu.List className="center shadow-blackA7 m-0 flex list-none rounded-[6px] bg-transparent p-1 shadow-[0_2px_10px]">
         {MENU_LIST.map((menuItem) => (
           <NavigationMenu.Item key={menuItem.name}>
             <Link
@@ -19,15 +19,7 @@ const NavMenuBar = () => {
             </Link>
           </NavigationMenu.Item>
         ))}
-
-        <NavigationMenu.Indicator className="data-[state=visible]:animate-fadeIn data-[state=hidden]:animate-fadeOut top-full z-[1] flex h-[10px] items-end justify-center overflow-hidden transition-[width,transform_250ms_ease]">
-          <div className="relative top-[70%] h-[10px] w-[10px] rotate-[45deg] rounded-tl-[2px] bg-white" />
-        </NavigationMenu.Indicator>
       </NavigationMenu.List>
-
-      <div className="perspective-[2000px] absolute top-full left-0 flex w-full justify-center">
-        <NavigationMenu.Viewport className="data-[state=open]:animate-scaleIn data-[state=closed]:animate-scaleOut relative mt-[10px] h-[var(--radix-navigation-menu-viewport-height)] w-full origin-[top_center] overflow-hidden rounded-[6px] bg-white transition-[width,_height] duration-300 sm:w-[var(--radix-navigation-menu-viewport-width)]" />
-      </div>
     </NavigationMenu.Root>
   );
 };
