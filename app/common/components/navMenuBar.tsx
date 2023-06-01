3b31338ac6f2a8a3da1024e6a0a4a1b23e8e8a9d
Sayak Dutta <sayak_dutta@intuit.com>
2023-06-02 02:38:17 +0530

    fix: use next link for menu links

--- app/common/components/navMenuBar.tsx
+++ app/common/components/navMenuBar.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React from 'react';
+import Link from 'next/link';
 import * as NavigationMenu from '@radix-ui/react-navigation-menu';
 import { MENU_LIST } from '../constants';
 
@@ -10,12 +11,12 @@ const NavMenuBar = () => {
       <NavigationMenu.List className="center shadow-blackA7 m-0 flex list-none rounded-[6px] bg-white p-1 shadow-[0_2px_10px]">
         {MENU_LIST.map((menuItem) => (
           <NavigationMenu.Item key={menuItem.name}>
-            <NavigationMenu.Link
+            <Link
               className="text-violet11 hover:bg-violet3 focus:shadow-violet7 block select-none rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none no-underline outline-none focus:shadow-[0_0_0_2px]"
               href={menuItem.path}
             >
               {menuItem.name}
-            </NavigationMenu.Link>
+            </Link>
           </NavigationMenu.Item>
         ))}
 
