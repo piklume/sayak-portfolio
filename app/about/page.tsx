7e1ad8e2744bc39c87848ace6c1c24aff491d54b
Sayak Dutta <sayak_dutta@intuit.com>
2023-06-02 23:36:18 +0530

    add about me card

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,11 @@
+import ContentCard from '../common/components/contentCard';
+
 export default function About() {
+  const content =
+    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores aperiam consectetur blanditiis doloribus laborum laboriosam officiis quidem illum vero. Nemo commodi facilis eos quo aliquam possimus? Consequuntur recusandae velit magni culpa eligendi illum voluptas nemo quibusdam molestias praesentium? Optio aspernatur nostrum quam voluptate aperiam nam aliquam, aliquid expedita excepturi magni fugiat, asperiores nobis dolor modi et odit non suscipit! Temporibus illo corporis laudantium magni necessitatibus similique voluptate commodi, blanditiis sint rem totam, maxime qui aperiam culpa minus ab eligendi. Aliquam dolorum esse provident doloremque quibusdam tempore. Quaerat suscipit enim ab, inventore provident quia quas illo facere! Quod nulla fugit magni.';
   return (
-    <div className="flex flex-col gap-y-8 items-center justify-center h-screen">
-      This is my about page
+    <div className="mt-8">
+      <ContentCard title="About me" content={content} />
     </div>
   );
 }
---
+++ app/common/components/contentCard.tsx
@@ -0,0 +1,15 @@
+type props = {
+  title: string;
+  content: string;
+  children?: React.ReactNode;
+};
+
+export default function ContentCard({ title, content, children }: props) {
+  return (
+    <div className="p-4 border-2 rounded-xl font-sans bg-slate-50/30 text-slate-700 hover:shadow-inner">
+      <h2 className="text-2xl">{title}</h2>
+      <p className="text-base mt-6">{content}</p>
+      <p className="text-base mt-1">{children}</p>
+    </div>
+  );
+}
