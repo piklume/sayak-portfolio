b739c70545b5ecda6dba48876d621ec34c4e255e
Sayak Dutta <sayak_dutta@intuit.com>
2023-06-06 21:53:53 +0530

    feat: initial add projects card

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,4 +1,6 @@
 import TextHeader from '../common/components/textHeader';
+import { PROJECTS_DATA_LIST } from '../common/constants';
+import ProjectCard from './components/projectCard';
 import ProjectsCarousel from './components/projectsCarousel';
 
 export default function Projects() {
@@ -6,6 +8,11 @@ export default function Projects() {
     <div className="mt-8 flex flex-col">
       <ProjectsCarousel />
       <TextHeader>Projects</TextHeader>
+      <div className="grid gap-4 grid-cols-1 md:grid-cols-4">
+        {PROJECTS_DATA_LIST.map(({ name, title, shortDescription }) => (
+          <ProjectCard key={name} title={title} content={shortDescription} />
+        ))}
+      </div>
     </div>
   );
 }
