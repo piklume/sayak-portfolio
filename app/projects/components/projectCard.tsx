f8cd465af1608364bfe2876ac38eb9dc60007a23
Sayak Dutta <sayak_dutta@intuit.com>
2023-06-09 02:52:03 +0530

    fix: navigate to project page on card click

--- app/projects/components/projectCard.tsx
+++ app/projects/components/projectCard.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Link from 'next/link';
 import { Project } from '@/app/common/types';
 import { PROJECTS_ICONS_LIST } from '../constants';
 
@@ -9,14 +10,16 @@ export default function ProjectCard({ project }: { project: Project }) {
   const Icon: any = PROJECTS_ICONS_LIST[id as keyof typeof PROJECTS_ICONS_LIST];
 
   return (
-    <div className="w-fit h-auto max-w-xs py-2 pl-0 pr-1 border-2 rounded-xl font-sans bg-slate-50/30 text-slate-700 hover:shadow-inner">
-      <div className="flex flex-row items-center">
-        {Icon}
-        <div className="basis-3/4">
-          <h2 className="text-lg font-semibold">{title}</h2>
-          <p className="text-base mt-2">{shortDescription}</p>
+    <Link href={`/projects/${id}`}>
+      <div className="w-fit h-auto max-w-xs py-2 pl-0 pr-1 border-2 rounded-xl font-sans bg-slate-50/30 text-slate-700 hover:shadow-inner">
+        <div className="flex flex-row items-center">
+          {Icon}
+          <div className="basis-3/4">
+            <h2 className="text-lg font-semibold">{title}</h2>
+            <p className="text-base mt-2">{shortDescription}</p>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
